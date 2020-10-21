b54455c5ff58f51f8989626d2540306b371d4737
KoTubA <szymon.kotarba988@gmail.com>
2020-10-21 15:54:48 +0200

    Update solution

--- Lista zadan nr 1/cw_12/main.c
+++ Lista zadan nr 1/cw_12/main.c
@@ -5,14 +5,10 @@ int main()
 {
     int licznik1, licznik2, mianownik1, mianownik2, wynik_licznik, wynik_mianownik;
     printf("Podaj pierwszy ulamek w formacie licznik/mianownik: \n");
-    scanf("%d", &licznik1);
-    printf("/\n");
-    scanf("%d", &mianownik1);
+    scanf("%d/%d", &licznik1, &mianownik1);
 
     printf("Podaj drugi ulamek w formacie licznik/mianownik: \n");
-    scanf("%d", &licznik2);
-    printf("/\n");
-    scanf("%d", &mianownik2);
+    scanf("%d/%d", &licznik2, &mianownik2);
 
     wynik_licznik = (licznik1 * mianownik2) + (licznik2 * mianownik1);
     wynik_mianownik = mianownik1 * mianownik2;
